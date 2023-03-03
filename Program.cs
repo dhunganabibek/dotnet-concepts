72563347f00b1bb0f352cdc31e497dd04d2db4cc
dhunganabibek <Dhunganabibek2016@gmail.com>
2023-03-02 22:13:18 -0600

    if else part added

--- Program.cs
+++ Program.cs
@@ -4,17 +4,34 @@
 
         public static void Main()
         {
-            Console.WriteLine("Hello, Place");
-            var list = new List<Int64> { 1, 2, 3, 4, 5 };
-            var dict = new Dictionary<string, string>();
+            //Console.WriteLine("Hello, Place");
+            //var list = new List<Int64> { 1, 2, 3, 4, 5 };
+            //var dict = new Dictionary<string, string>();
 
-            dict["name"] = "Bibek";
-            dict["age"] = "15";
+            //dict["name"] = "Bibek";
+            //dict["age"] = "15";
 
-            Console.WriteLine(list);
+            //Console.WriteLine(list);
 
-            Console.WriteLine("Hello, test2");
+            //Console.WriteLine("Hello, test2");
 
+            Console.Write("x:");
+            var x = int.Parse(Console.ReadLine());
+
+            Console.Write("y:");
+            var y = int.Parse(Console.ReadLine());
+
+            Console.Write("Operation:");
+            var operation = Console.ReadLine();
+
+            if(operation == "+")
+            {
+                Console.WriteLine($"{x} + {y} = {x+y}");
+            }
+            else if(operation == "-")
+            {
+                Console.WriteLine($"{x} - {y} = {x - y}");
+            }
 
         }
 
