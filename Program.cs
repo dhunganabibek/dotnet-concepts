2b459168d700599a882b17ed9485fb9c533b1157
dhunganabibek <Dhunganabibek2016@gmail.com>
2024-07-20 21:25:46 -0500

    working with target type new expression

--- Program.cs
+++ Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Testing target type new expression
+TargetTypeNewExpression.NewExpression();
