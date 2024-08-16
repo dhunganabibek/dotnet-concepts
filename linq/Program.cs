cc5a3111a263d3f2808c6081f07fc41572851a8b
dhunganabibek <Dhunganabibek2016@gmail.com>
2024-08-15 21:26:15 -0500

    working with Newtonsoft.json

--- linq/Program.cs
+++ linq/Program.cs
@@ -1,21 +1,43 @@
-﻿using Dumpify;
+﻿using System.Security.Cryptography;
+using Dumpify;
+using Newtonsoft.Json;
 
-IEnumerable<int> collection = [1, 2, 3, 4, 5];
+// IEnumerable<int> collection = [1, 2, 3, 4, 5];
 
-// Where for filtering
-collection.Where(x => x > 5).Dump();
+// // Where for filtering
+// collection.Where(x => x > 5).Dump();
 
-// ofType to get of specific type
-collection.OfType<int>().Dump();
+// // ofType to get of specific type
+// collection.OfType<int>().Dump();
 
 
-// skip 
-collection.Skip(5).Dump();
+// // skip 
+// collection.Skip(5).Dump();
 
-// take that much number of element
-collection.Take(19).Dump();
+// // take that much number of element
+// collection.Take(19).Dump();
 
-// skipWhile and TakeWhile (filter until false)
+// // skipWhile and TakeWhile (filter until false)
 
-// Select
-collection.Select((x, i) => $"{x.ToString()}---- {i}").Dump();
+// // Select
+// collection.Select((x, i) => $"{x.ToString()}---- {i}").Dump();
+var data = @"
+[
+    {""name"":""Bibek"", ""age"":25, ""occupation"":""Engineer""},
+    {""name"":""Santona"", ""age"":24, ""occupation"":""Designer""},
+    {""name"":""John"", ""age"":30, ""occupation"":""Teacher""},
+    {""name"":""Alice"", ""age"":28, ""occupation"":""Doctor""},
+    {""name"":""Michael"", ""age"":35, ""occupation"":""Lawyer""},
+    {""name"":""Emily"", ""age"":22, ""occupation"":""Student""},
+    {""name"":""David"", ""age"":40, ""occupation"":""Manager""},
+    {""name"":""Sophia"", ""age"":27, ""occupation"":""Nurse""},
+    {""name"":""Daniel"", ""age"":32, ""occupation"":""Architect""},
+    {""name"":""Olivia"", ""age"":29, ""occupation"":""Writer""}
+]";
+
+;
+
+var persons = JsonConvert.DeserializeObject<Person[]>(data);
+Console.WriteLine(JsonConvert.SerializeObject(persons, Formatting.Indented));
+
+record Person(string name, int age, string occupation);
