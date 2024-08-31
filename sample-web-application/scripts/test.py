d56213731358e66c9af856247ff2d4a3ba8a4ba8
Bibek Dhungana <dhunganabibek2016@gmail.com>
2024-08-31 17:00:56 -0500

    made api call using request

--- sample-web-application/scripts/test.py
+++ sample-web-application/scripts/test.py
@@ -1,1 +1,9 @@
-print("Hello World!!")
+import requests
+import json
+
+
+response = requests.get('http://jsonplaceholder.typicode.com/posts')
+
+result = response.json()
+
+print(json.dumps(result, indent=4))
