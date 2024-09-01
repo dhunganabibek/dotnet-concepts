d73f2b88c81bb2c5f9816c6f464549249385dadf
Bibek Dhungana <dhunganabibek2016@gmail.com>
2024-09-01 14:26:27 -0500

    WIP

--- sample-web-application/scripts/test.py
+++ sample-web-application/scripts/test.py
@@ -1,9 +1,5 @@
-import requests
-import json
 
 
-response = requests.get('http://jsonplaceholder.typicode.com/posts')
-
-result = response.json()
-
-print(json.dumps(result, indent=4))
+def main ():
+    print("from test.py")
+    
