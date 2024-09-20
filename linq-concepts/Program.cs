4f79a60403186d0c8c2f01285dd8eaaa022633ab
Bibek Dhungana <dhunganabibek2016@gmail.com>
2024-09-20 16:26:40 -0500

    new solution file created and project added for auto complete

--- linq-concepts/Program.cs
+++ linq-concepts/Program.cs
@@ -1,9 +1,16 @@
 ﻿using System.Collections.Generic;
+using System.Collections;
 using Dumpify;
 
 IEnumerable<int> collection = [1, 2, 3, 4, 5];
 
+ArrayList al = new ArrayList();
+al.Add(1);
+al.Add(2);
+al.Add(3);
 
-collection.Count().Dump();
+System.Console.WriteLine(al);
 
-collection.Select((x, i) => x * 2).Dump();
+// collection.Count().Dump();
+
+// collection.Select((x, i) => x * 2).Dump();
