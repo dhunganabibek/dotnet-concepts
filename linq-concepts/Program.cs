5436aa519273e158b1f70f16bee00946eb02d671
Bibek Dhungana <dhunganabibek2016@gmail.com>
2024-09-22 12:26:25 -0500

    working with anonymous class

--- linq-concepts/Program.cs
+++ linq-concepts/Program.cs
@@ -3,10 +3,55 @@ using System.Collections;
 using Dumpify;
 
 
+#region Anonymous Types
+
+
+Student s = new Student();
+System.Console.WriteLine(s.Name);
+
+class Student
+{
+    int Id;
+    public string Name
+    {
+        get
+        {
+            return "John";
+        }
+        set
+        {
+            Name = value;
+        }
+    }
+    int Age;
+};
+#endregion
+
+
 
 #region IComparable and IComparer
 
+// List<Student> list = new List<Student> {
+//         new Student { Id = 1, Name = "John", Age = 20 },
+//         new Student { Id = 2, Name = "Jane", Age = 22 },
+//         new Student { Id = 3, Name = "Doe", Age = 24 },
+//         new Student { Id = 4, Name = "Smith", Age = 26 },
+//         new Student { Id = 5, Name = "Doe", Age = 28 }
+//         };
 
+
+// foreach (var item in list)
+// {
+//     Console.WriteLine(item.Name);
+// }
+
+
+// class Student
+// {
+//     public int Id { get; set; }
+// public string Name { get; set; }
+// public int Age { get; set; }
+// }
 
 #endregion
 
