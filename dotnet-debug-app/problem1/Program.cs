76cf30ba422b2b3b6ef5563e6a383d337a21ce8c
dhunganabibek <Dhunganabibek2016@gmail.com>
2024-10-24 21:19:44 -0500

    lambda and delegate

--- dotnet-debug-app/problem1/Program.cs
+++ dotnet-debug-app/problem1/Program.cs
@@ -1,8 +1,11 @@
-﻿var num = 20;
-
-bool isOdd(int num)
+﻿List<int> list = [1, 2, 3];
+// Function declartion
+bool isOddFD(int num)
 {
-    return num % 2 == 1;
+    return num % 2 == 0;
 }
 
-Console.WriteLine(isOdd(20));
+//function expression
+Func<int, bool> isOddFE = num => num % 2 == 0;
+
+list.Where(isOddFD);
