0d8ac0b4bb12cde56759def4cbce653843f5f59b
dhunganabibek <Dhunganabibek2016@gmail.com>
2025-02-23 21:22:41 -0600

    WIP

--- unit-testing/demo.Test/UnitTest1.cs
+++ unit-testing/demo.Test/UnitTest1.cs
@@ -13,7 +13,7 @@ public class UnitTest
         // Act
         var val1 = 50000;
         var val2 = 50000;
-        var positiveResult = val1 + val2;
+        var expectedPositiveResult = val1 + val2;
         var result = calculator.Add(val1, val2);
 
 
@@ -23,14 +23,24 @@ public class UnitTest
         var result3 = calculator.Add(0, 10);
         // Assert
         //Assert.Equal(10000, result);
-        Assert.True(result == positiveResult, $"Expected , but got {result}. Possible miscalculation in the addition.");
+        Assert.True(result == expectedPositiveResult, $"Expected , but got {result}. Possible miscalculation in the addition.");
         Assert.Equal(5, result2);
         Assert.Equal(10, result3);
     }
 
-    [Fact]
-    public void TestSub()
+    [Theory]
+    [InlineData(10, 5, 5)]
+    [InlineData(2, 3, -1)]
+    [InlineData(0, 0, 0)]
+    public void TestSub(int input1, int input2, int expectedResult)
     {
-        System.Console.WriteLine("This is from test 2");
+        // Arrange 
+        var calculator = new Calculator();
+
+        // act
+        var result = calculator.Sub.Compile()(input1, input2);
+
+        // Assert 
+        Assert.True(result == expectedResult);
     }
 }
