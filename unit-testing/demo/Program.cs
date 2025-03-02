8cc1a202a479257607a340a8da71a967c9ee69ed
dhunganabibek <Dhunganabibek2016@gmail.com>
2025-03-01 23:37:35 -0600

    Reference vs Value type

--- unit-testing/demo/Program.cs
+++ unit-testing/demo/Program.cs
@@ -1,14 +1,19 @@
-﻿using demo;
-using System;
+﻿Test t1 = new Test(10, 20);
+Test t2 = t1;
+t2.a = 500;
 
-Console.WriteLine("Hello, World!");
-
-
-Calculator c = new Calculator();
-var myAdd = c.Add;
-var mySub = c.Sub.Compile();
+System.Console.WriteLine(t2.a);
 
 
-Console.WriteLine(mySub(4, 7) == 11);
-Console.WriteLine(myAdd(4, 7));
+public struct Test
+{
 
+    public int a { get; set; }
+    public int b { get; set; }
+
+    public Test(int a, int b)
+    {
+        this.a = a;
+        this.b = b;
+    }
+}
